5749e483123cae2233fb0a8d24a3b511fda06708
Karen Garcia <kgarcia@i3screen.com>
2020-02-04 20:40:02 -0700

    Updates

---
+++ precache-manifest.68112829c2ab973a91bdd1ce528887ca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303758671b66d0a12c630c6577beefd0",
+    "url": "/BomdigityPortfolio/index.html"
+  },
+  {
+    "revision": "5ce60b7b57ea60ad824e",
+    "url": "/BomdigityPortfolio/static/css/main.254765c3.chunk.css"
+  },
+  {
+    "revision": "905f7843df5db05244bb",
+    "url": "/BomdigityPortfolio/static/js/2.d2a539ac.chunk.js"
+  },
+  {
+    "revision": "5ce60b7b57ea60ad824e",
+    "url": "/BomdigityPortfolio/static/js/main.21cbd00a.chunk.js"
+  },
+  {
+    "revision": "a87eccbc1e983b0477ab",
+    "url": "/BomdigityPortfolio/static/js/runtime~main.3f055328.js"
+  },
+  {
+    "revision": "fecba441386c34c2f150877a809bd0a4",
+    "url": "/BomdigityPortfolio/static/media/AddressBookPic.fecba441.png"
+  },
+  {
+    "revision": "7a737d02eb03b76e030334a473132d90",
+    "url": "/BomdigityPortfolio/static/media/Bomdigity.7a737d02.gif"
+  },
+  {
+    "revision": "bab4ad0731dd5a168ce9e11289d7fe78",
+    "url": "/BomdigityPortfolio/static/media/ColorsRedux_10.2019.bab4ad07.gif"
+  },
+  {
+    "revision": "9bd0906744b9d7f2acc867ba2f268ab8",
+    "url": "/BomdigityPortfolio/static/media/FIRE.9bd09067.png"
+  },
+  {
+    "revision": "0a0c9e0c9f1d7626a0fe6ccc5de0b2d4",
+    "url": "/BomdigityPortfolio/static/media/GitFitter.0a0c9e0c.gif"
+  },
+  {
+    "revision": "2099b4832a54e318deaaa2908f793138",
+    "url": "/BomdigityPortfolio/static/media/Karen_freeCodeCamp_Certificate_Responsive_Web_Design_2.13.2019.2099b483.png"
+  },
+  {
+    "revision": "7f28071e5bee2a6f88689506534d2d30",
+    "url": "/BomdigityPortfolio/static/media/Number_Guesser.7f28071e.gif"
+  },
+  {
+    "revision": "56952159168e1c475dc1edeb1a7be781",
+    "url": "/BomdigityPortfolio/static/media/React_Website_ES6_8.1.2019.56952159.png"
+  },
+  {
+    "revision": "f31a1bc9a903382998c4d47eb4c31462",
+    "url": "/BomdigityPortfolio/static/media/businessFinancial.f31a1bc9.png"
+  },
+  {
+    "revision": "230b6c8b8f16c40b30484e727a311da5",
+    "url": "/BomdigityPortfolio/static/media/checkwritting.230b6c8b.png"
+  },
+  {
+    "revision": "bd82fb9a7ee7894d890bdf0d6bedc8bf",
+    "url": "/BomdigityPortfolio/static/media/flashcard-starter.bd82fb9a.gif"
+  },
+  {
+    "revision": "f00add6dbcf78651995998cfebba726f",
+    "url": "/BomdigityPortfolio/static/media/karenHero.f00add6d.jpg"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/BomdigityPortfolio/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/BomdigityPortfolio/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
